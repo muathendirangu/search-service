3d985b89ac1039c1ea6f9cbc051292a727527324
muathendirangu <muathe.ndirangu@gmail.com>
2020-09-16 21:12:00 +0300

    add bulk insert operation of documents

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olivere/elastic"
+	"github.com/teris-io/shortid"
+)
+
+const (
+	elasticIndexName = "documents"
+	elasticTypeName  = "document"
+)
+
+// Document type
+type Document struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	Content   string    `json:"content"`
+}
+
+// DocumentRequest represents single document
+type DocumentRequest struct {
+	Title   string `json:"title"`
+	Content string `json:"content"`
+}
+
+var (
+	elasticSearchClient *elastic.Client
+)
+
+func main() {
+	var err error
+	for {
+		elasticSearchClient, err := elastic.NewClient(elastic.SetURL("localhost:9200"), elastic.SetSniff(false))
+		if err != nil {
+			log.Println(err)
+			time.Sleep(3 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	r := gin.Default()
+	r.POST("/", createDocumentEndpoint)
+	if err = r.Run(":8000"); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func errorResponse(c *gin.Context, code int, err string) {
+	c.JSON(code, gin.H{
+		"error": err,
+	})
+}
+
+func createDocumentEndpoint(c *gin.Context) {
+	var docs []DocumentRequest
+	if err := c.BindJSON(&docs); err != nil {
+		errorResponse(c, http.StatusBadRequest, "Malformed request body")
+		return
+	}
+	bulk := elasticSearchClient.Bulk().Index(elasticIndexName).Type(elasticTypeName)
+	for _, d := range docs {
+		doc := Document{
+			ID:        shortid.MustGenerate(),
+			Title:     d.Title,
+			CreatedAt: time.Now().Local().UTC(),
+			Content:   d.Content,
+		}
+		bulk.Add(elastic.NewBulkIndexRequest().Id(doc.ID).Doc(doc))
+	}
+	if _, err := bulk.Do(c.Request.Context()); err != nil {
+		log.Println(err)
+		errorResponse(c, http.StatusInternalServerError, "Failed to create documents")
+		return
+	}
+	c.Status(http.StatusOK)
+}
